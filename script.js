8ae66d5230218697588ecddf64c27175f9e9b1bd
Kari Meghana <karimeghana02@gmail.com>
2025-08-31 14:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const select = document.getElementById('colorSelect');
+    const selectedIndex = select.selectedIndex;
+
+    if (selectedIndex >= 0) {
+        select.remove(selectedIndex);
+    } else {
+        alert('Please select a color to remove');
+    }
+}
